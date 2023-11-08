05637698a6432a4268d1a5f142946c01a5b9e312
Sude Demirtaş <115345132+sudemiirtas@users.noreply.github.com>
2023-11-08 09:58:19 +0300

    stdafx.h

---
+++ stdafx.h
@@ -0,0 +1,20 @@
+// stdafx.h : standart sistem için dosya dahil dosyaları,
+// veya projeye özel sık kullanılan dosyaları içerir, ancak
+// nadiren değiştirilir
+//
+
+#pragma once
+
+#include "targetver.h"
+
+#define WIN32_LEAN_AND_MEAN             // Nadiren kullanılan öğeleri Windows başlıklarından hariç tutun
+// Windows Header Files:
+#include <windows.h>
+
+// C RunTime Header Files
+#include <stdlib.h>
+#include <malloc.h>
+#include <memory.h>
+#include <tchar.h>
+
+
