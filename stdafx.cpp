6b04ded5307fef98a468603881a1a76cc24b1450
Sude Demirtaş <115345132+sudemiirtas@users.noreply.github.com>
2023-11-08 09:58:49 +0300

    stdafx.cpp

---
+++ stdafx.cpp
@@ -0,0 +1,8 @@
+// stdafx.cpp : yalnızca standart içerikleri içeren kaynak dosya
+// Paint_Programing.pch önceden derlenmiş başlık olacak
+// stdafx.obj önceden derlenmiş tür bilgisini içerecektir
+
+#include "stdafx.h"
+
+// TODO: STDAFX.H'de ihtiyaç duyduğunuz ek başlıklara referans verin
+// ve bu dosyada değil
