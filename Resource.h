1b9436651524cb24b96539963fd339784c0f84f6
İclal Çengel <115634785+iclalcengel@users.noreply.github.com>
2023-11-08 15:46:02 +0300

    Resource.h

---
+++ Resource.h
@@ -0,0 +1,87 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by Paint_Programing.rc
+//
+#define IDC_MYICON                      2
+#define IDD_PAINT_PROGRAMING_DIALOG     102
+#define IDS_APP_TITLE                   103
+#define IDD_ABOUTBOX                    103
+#define IDM_ABOUT                       104
+#define IDM_EXIT                        105
+#define IDI_PAINT_PROGRAMING            107
+#define IDI_SMALL                       108
+#define IDC_PAINT_PROGRAMING            109
+#define IDR_MAINFRAME                   128
+#define IDD_DIALOG1                     129
+#define ID_NEWMENU1_NEWWINDOW           32771
+#define ID_COMMAND_1                    32773
+#define ID_LineTool                     32777
+#define ID_RectTool                     32778
+#define ID_CircleTool                   32779
+#define ID_TextTool                     32780
+#define ID_BLACK                        32789
+#define ID_GRAY                         32790
+#define ID_RED                          32791
+#define ID_GREEN                        32792
+#define ID_BLU                          32793
+#define ID_CYAN                         32794
+#define ID_YELLOW                       32795
+#define ID_Magenta                      32797
+#define ID_32799                        32799
+#define ID_NEW_FILE                     32800
+#define ID_32801                        32801
+#define ID_32802                        32802
+#define ID_32803                        32803
+#define ID_OPEN_FILE                    32804
+#define ID_SAVE                         32805
+#define ID_SAVE_AS                      32806
+#define ID_32807                        32807
+#define ID_32808                        32808
+#define ID_Menu                         32809
+#define ID_32810                        32810
+#define ID_32811                        32811
+#define ID_32812                        32812
+#define ID_32813                        32813
+#define ID_32814                        32814
+#define ID_32815                        32815
+#define ID_32816                        32816
+#define ID_32817                        32817
+#define ID_32818                        32818
+#define ID_32819                        32819
+#define ID_32820                        32820
+#define ID_SelectTool                   32821
+#define ID_BG_Transparent               32822
+#define ID_BG_GRAY                      32823
+#define ID_BG_RED                       32824
+#define ID_BG_GREEN                     32825
+#define ID_BG_BLU                       32826
+#define ID_BG_CYAN                      32827
+#define ID_BG_YELLOW                    32828
+#define ID_BG_Magenta                   32829
+#define ID_Wide1                        32830
+#define ID_Wide2                        32831
+#define ID_Wide3                        32832
+#define ID_Wide4                        32833
+#define ID_Wide5                        32834
+#define ID_Copy                         32835
+#define ID_Cut                          32836
+#define ID_Paste                        32837
+#define ID_Delete                       32838
+#define ID_Undo                         32839
+#define ID_Redo                         32840
+#define ID_ACCELERATOR32846             32846
+#define ID_Debug                        32847
+#define ID_DebugMode                    32849
+#define IDC_STATIC                      -1
+
+// Yeni nesneler için sonraki varsayılan değerler
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NO_MFC                     1
+#define _APS_NEXT_RESOURCE_VALUE        130
+#define _APS_NEXT_COMMAND_VALUE         32906
+#define _APS_NEXT_CONTROL_VALUE         1000
+#define _APS_NEXT_SYMED_VALUE           110
+#endif
+#endif
